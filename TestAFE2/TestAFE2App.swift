b4041caa87e4e03535778c11904974050c81a860
Abdellah2Success <a.skoundri@2success.fr>
2025-09-22 22:17:16 +0200

    Initial Commit

---
+++ TestAFE2/TestAFE2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestAFE2App.swift
+//  TestAFE2
+//
+//  Created by Abdellah Skoundri on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAFE2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
