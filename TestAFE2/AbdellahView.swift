976ca56105afd591747ae20d284a9b3a392892ec
Abdellah2Success <a.skoundri@2success.fr>
2025-09-23 13:38:12 +0200

    ajout feature

---
+++ TestAFE2/AbdellahView.swift
@@ -0,0 +1,18 @@
+//
+//  AbdellahView.swift
+//  TestAFE2
+//
+//  Created by Abdellah Skoundri on 23/09/2025.
+//
+
+import SwiftUI
+
+struct AbdellahView: View {
+    var body: some View {
+        Text("la soeur a dim en 4K")
+    }
+}
+
+#Preview {
+    AbdellahView()
+}
