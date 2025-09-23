e6b418b0e653bc5165a2552ffa9f072b506a4244
DimitriKft <dimitri.kft@gmail.com>
2025-09-23 13:29:17 +0200

    yoooo

---
+++ TestAFE2/DimView.swift
@@ -0,0 +1,19 @@
+//
+//  DimView.swift
+//  TestAFE2
+//
+//  Created by Dimitri on 23/09/2025.
+//
+
+import SwiftUI
+
+struct DimView: View {
+    var body: some View {
+        Text("Dim BG du 31!!!!")
+            .font(.title)
+    }
+}
+
+#Preview {
+    DimView()
+}
